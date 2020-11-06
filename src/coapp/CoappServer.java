1b19b3c83c364069c0df0e158a7e34d90b63c73b
“Salinda” <h.salinda@gmail.com>
2020-11-06 12:45:24 +0100

    Initial commit of the IOT labs project.

---
+++ src/coapp/CoappServer.java
@@ -0,0 +1,8 @@
+package coapp;
+
+public class CoappServer extends Thread {
+
+  public void run(){
+    System.out.println("Started");
+  }
+}
---
+++ src/main/ServerApplication.java
@@ -0,0 +1,12 @@
+package main;
+
+import coapp.CoappServer;
+
+public class ServerApplication {
+
+  public static void main(String[] args) {
+
+    CoappServer coappServer = new CoappServer();
+    coappServer.start();
+  }
+}
